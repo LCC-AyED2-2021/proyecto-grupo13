31f96a2b93605fe2e1a7d937dd05924e78393abd
Aner Lucero <4rgento@gmail.com>
2021-06-26 22:11:17 -0300

    Profile creation

--- prof.py
+++ prof.py
@@ -4,8 +4,10 @@ import cProfile
 import pstats
 from pstats import SortKey
 
-cProfile.run('personal_library.search(\'books\', \'network\')', 'qStats')
-
+cProfile.run('personal_library.create(\'books\')', 'cStats')
 p = pstats.Stats('qStats')
+p.strip_dirs().sort_stats(SortKey.TIME).print_stats()
 
+cProfile.run('personal_library.search(\'books\', \'network\')', 'qStats')
+p = pstats.Stats('cStats')
 p.strip_dirs().sort_stats(SortKey.TIME).print_stats()
